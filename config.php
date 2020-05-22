1c15530eac4f6643b052447451cb2910ec588089
Gustavo Pessoa <gupessoa@live.com>
2020-05-22 16:29:14 -0300

    Criação de estrutura básica para criar a fundação do mvc

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+	require 'environment.php';
+
+	$config = array();
+
+	if(ENVIRONMENT == 'development'){
+		define('BASE_URL', 'http://localhost/mvc.dev/');
+		
+		$config['dbname'] = 'mvc';
+		$config['host'] = 'localhost';
+		$config['dbuser'] = 'gupessoa';
+		$config['dbpass'] = 'gust@v000';
+	}else{
+		define('BASE_URL', 'http://localhost/mvc.dev/');
+		
+		$config['dbname'] = 'mvc';
+		$config['host'] = 'localhost';
+		$config['dbuser'] = 'gupessoa';
+		$config['dbpass'] = 'gust@v000';
+	}
+
+	global $db;
+
+	try{
+		$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+	}catch(PDOException $e){
+		echo "ERRO: ".e->getMessage();
+		exit;
+	}
